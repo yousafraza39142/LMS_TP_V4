b567f4d4b58700c96bc28e3e7609d751b8e64a6b
yousafraza39142 <31831225+yousafraza39142@users.noreply.github.com>
2020-05-11 14:58:02 +0500

    1)Assignment and Project Done and LAB Done and Also before Pull

--- src/app/main/mark-assessment/lab/students-lab/students-lab.component.ts
+++ src/app/main/mark-assessment/lab/students-lab/students-lab.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {NgForm} from '@angular/forms';
 import {CourseModal} from '../../../../shared/course.modal';
 import {SectionModal} from '../../../../shared/SectionModal';
@@ -7,6 +7,11 @@ import {AppState} from '../../../../store/app.reducers';
 import {LabModal} from '../../../../shared/LabModal';
 import {studentLabsTable} from '../store/lab.component.reducer';
 import {SlideInFromLeft} from '../../../../transitions';
+import {AssessmentTable} from '../../assignments/student-assignment/student-assignment.component';
+import {AssignmentApiService} from '../../assignments/assignment-services/assignment-api.service';
+import {MarkAssessmentService} from '../../mark-assessment.service';
+import {AssignmentModal} from '../../../../shared/AssignmentModal';
+import {AssessmentTypes} from '../../../../shared/AssessmentTypes';
 
 @Component({
   selector: 'app-students-lab',
@@ -18,15 +23,24 @@ import {SlideInFromLeft} from '../../../../transitions';
 })
 export class StudentsLabComponent implements OnInit {
   totalMarks: number;
-  studentsLabTable: studentLabsTable[];
-  labs: LabModal[];
-  courses: CourseModal[];
-  sections: SectionModal[];
+  studentsLabTable: Array<AssessmentTable>;
+  labs: Array<LabModal>;
+  courses: Array<CourseModal>;
+  sections: Array<SectionModal>;
+  @ViewChild('c') selectCourse: ElementRef;
+  @ViewChild('s') selectSection: ElementRef;
+  @ViewChild('l') selectLab: ElementRef;
 
-  constructor(private store: Store<AppState>) { }
-
+  constructor(private store: Store<AppState>,
+              private assignmentApiService: AssignmentApiService,
+              private markAssessmentService: MarkAssessmentService) {
+    this.courses = new Array<CourseModal>();
+    this.sections = new Array<SectionModal>();
+    this.labs = new Array<LabModal>();
+    this.studentsLabTable = new Array<AssessmentTable>();
+  }
   ngOnInit(): void {
-    this.store.select('fromLab').subscribe(
+    /*this.store.select('fromLab').subscribe(
       state => {
         this.totalMarks = state.data.total_marks;
         this.studentsLabTable = state.data.students_labs;
@@ -38,10 +52,110 @@ export class StudentsLabComponent implements OnInit {
         this.courses = state.courses;
         this.sections = state.sections;
       }
+    );*/
+    this.markAssessmentService.getCourseForTeacher(1).subscribe(
+      data => {
+        // @ts-ignore
+        for (const course: { SUB_NM: string } of data) {
+          this.courses.push(new CourseModal(course.SUB_NM));
+        }
+        if (this.courses.length > 0) {
+          this.markAssessmentService.getSectionsForTeacherinCourse(1, this.courses[0].courseTitle).subscribe(
+            section => {
+              // @ts-ignore
+              for (const sec: { SECTION: string } of section) {
+                this.sections.push(new SectionModal(sec.SECTION));
+              }
+              if (this.sections.length > 0) {
+                console.log('Course', this.courses[0]);
+                console.log('section', this.sections[0]);
+                this.assignmentApiService.getAssignmentList(this.sections[0].sectionTitle,
+                  this.courses[0].courseTitle,
+                  AssessmentTypes.LAB).subscribe(
+                  labs => {
+                    // @ts-ignore
+                    for (const lab of labs) {
+                      this.labs.push(new LabModal(lab.ASSIGNMENT_TITLE));
+                    }
+                  }
+                );
+              }
+            }
+          );
+        }
+      }
     );
   }
 
   onSubmit(form: NgForm) {
-    console.log(form.value);
+    this.assignmentApiService.getAssignmentsListOfStudents(this.selectSection.nativeElement.value,
+      this.selectCourse.nativeElement.value, this.selectLab.nativeElement.value, AssessmentTypes.LAB).subscribe(
+      students => {
+        console.log(students);
+        const list = students as AssessmentTable[];
+        if (list.length > 0) {
+          this.totalMarks = students[0].ASS_TOT_MRKS;
+        }
+        // @ts-ignore
+        for (const std of students) {
+          console.log(std);
+          this.studentsLabTable.push(std);
+        }
+      }
+    );
+  }
+
+  OnCourseChange(c: HTMLSelectElement) {
+    // Clear previous sections
+    for (const sec of this.sections) {
+      this.sections.pop();
+    }
+
+    // Fetch New Sections on Course Change
+    this.markAssessmentService.getSectionsForTeacherinCourse(1, c.value).subscribe(
+      section => {
+        // @ts-ignore
+        for (const sec: { SECTION: string } of section) {
+          this.sections.push(new SectionModal(sec.SECTION));
+        }
+      }
+    );
+
+    this.clearLab();
+    // @ts-ignore
+    // tslint:disable-next-line:max-line-length
+    this.assignmentApiService.getAssignmentList(this.selectSection.nativeElement.value, this.selectCourse.nativeElement.value, AssessmentTypes.LAB).subscribe(
+      labs => {
+        // @ts-ignore
+        for (const lab of labs) {
+          this.labs.push(new LabModal(lab.ASSIGNMENT_TITLE));
+        }
+      }
+    );
+  }
+
+
+  OnSectionChange(s: HTMLSelectElement) {
+    // Clear Assignments Drop Down
+    this.clearLab();
+
+    console.log();
+    console.log(this.selectCourse);
+    // @ts-ignore
+    // tslint:disable-next-line:max-line-length
+    this.assignmentApiService.getAssignmentList(this.selectSection.nativeElement.value, this.selectCourse.nativeElement.value, AssessmentTypes.LAB).subscribe(
+      labs => {
+        // @ts-ignore
+        for (const lab of labs) {
+          this.labs.push(new LabModal(lab.ASSIGNMENT_TITLE));
+        }
+      }
+    );
+  }
+
+  private clearLab() {
+    for (const lab of this.labs) {
+      this.labs.pop();
+    }
   }
 }
--- src/app/main/mark-assessment/lab/upload-lab/upload-lab.component.ts
+++ src/app/main/mark-assessment/lab/upload-lab/upload-lab.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {NgForm} from '@angular/forms';
 import {CourseModal} from '../../../../shared/course.modal';
 import {SectionModal} from '../../../../shared/SectionModal';
 import {Store} from '@ngrx/store';
 import {AppState} from '../../../../store/app.reducers';
 import {SlideInFromLeft} from '../../../../transitions';
+import {MarkAssessmentService} from '../../mark-assessment.service';
 
 @Component({
   selector: 'app-upload-lab',
@@ -16,22 +17,88 @@ import {SlideInFromLeft} from '../../../../transitions';
 })
 export class UploadLabComponent implements OnInit {
 
-  courses: CourseModal[];
-  sections: SectionModal[];
+  courses: Array<CourseModal>;
+  sections: Array<SectionModal>;
+  @ViewChild('c') selectCourse: ElementRef;
+  @ViewChild('s') selectSection: ElementRef;
 
-  constructor(private store: Store<AppState>) { }
-
+  constructor(private store: Store<AppState>, private markAssessmentService: MarkAssessmentService) {
+    this.courses = new Array<CourseModal>();
+    this.sections = new Array<SectionModal>();
+  }
   ngOnInit(): void {
+    /*this.store.select('fromAssignment').subscribe(
+      state => {
+        console.log(state.data.assignments);
+        this.data = state.data;
+      }
+    );
     this.store.select('fromMarkAssessment').subscribe(
       state => {
-        // console.log(state);
+        console.log(state);
         this.courses = state.courses;
         this.sections = state.sections;
+      }
+    );
+*/
+    // if Wanna dispatch own courses
+    /*this.info = {
+      courses: [
+        new CourseModal('YOousaf'),
+        new CourseModal('Dani'),
+        new CourseModal('Math'),
+        new CourseModal('Chinese'),
+        new CourseModal('Pk.Std')
+      ],
+        sections: [
+        new SectionModal('Section Dani'),
+        new SectionModal('Section B'),
+        new SectionModal('Section C'),
+        new SectionModal('Section E1'),
+        new SectionModal('Section Ali')
+      ]
+    };*/
+    // this.store.dispatch(new fromAssignmentActions.StoreData(this.info));
+
+    this.markAssessmentService.getCourseForTeacher(1).subscribe(
+      data => {
+        // @ts-ignore
+        for (const course: { SUB_NM: string } of data) {
+          this.courses.push(new CourseModal(course.SUB_NM));
+        }
+        if (this.courses.length > 0) {
+          this.markAssessmentService.getSectionsForTeacherinCourse(1, this.courses[0].courseTitle).subscribe(
+            section => {
+              // @ts-ignore
+              for (const sec: { SECTION: string } of section) {
+                this.sections.push(new SectionModal(sec.SECTION));
+              }
+            }
+          );
+        }
+      }
+    );
+  }
+  OnCourseChange(c: HTMLSelectElement) {
+    // Clear previous sections
+    for (const sec of this.sections) {
+      this.sections.pop();
+    }
+
+    // Fetch New Sections on Course Change
+    this.markAssessmentService.getSectionsForTeacherinCourse(1, c.value).subscribe(
+      section => {
+        // @ts-ignore
+        for (const sec: { SECTION: string } of section) {
+          this.sections.push(new SectionModal(sec.SECTION));
+        }
       }
     );
   }
 
   onSubmit(form: NgForm) {
-    console.log(form.value);
+
+    // Dont use ngForm values but use childView declared above to get values
+    // console.log(form.value);
   }
 }
