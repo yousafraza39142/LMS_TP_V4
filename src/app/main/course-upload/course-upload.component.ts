73b1998987aca5550314f33612130d5722f54b2b
yousafraza39142 <31831225+yousafraza39142@users.noreply.github.com>
2020-05-11 17:49:09 +0500

    Course Upload Drop-Downs Done

--- src/app/main/course-upload/course-upload.component.ts
+++ src/app/main/course-upload/course-upload.component.ts
@@ -1,10 +1,17 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {NgForm} from '@angular/forms';
 import {Store} from '@ngrx/store';
 import * as fromApp from '../../store/app.reducers';
 import * as fromCourseUpload from './store/course-upload.actions';
 import {CourseUpload, UploadResponse} from './store/course-upload.reducer';
 import {SlideInFromLeft} from '../../transitions';
+import {CourseModal} from '../../shared/course.modal';
+import {SectionModal} from '../../shared/SectionModal';
+import {AppState} from '../../store/app.reducers';
+import {AssignmentApiService} from '../mark-assessment/assignments/assignment-services/assignment-api.service';
+import {MarkAssessmentService} from '../mark-assessment/mark-assessment.service';
+import {AssignmentModal} from '../../shared/AssignmentModal';
+import {AssessmentTable} from '../mark-assessment/assignments/student-assignment/student-assignment.component';
 
 @Component({
   selector: 'app-course-upload',
@@ -20,19 +27,66 @@ export class CourseUploadComponent implements OnInit {
   info: CourseUpload;
   private file: any;
 
-  constructor(private store: Store<fromApp.AppState>) {
+
+  courses: Array<CourseModal>;
+  sections: Array<SectionModal>;
+  @ViewChild('c') selectCourse: ElementRef;
+  @ViewChild('s') selectSection: ElementRef;
+
+  constructor(private store: Store<AppState>,
+              private assignmentApiService: AssignmentApiService,
+              private markAssessmentService: MarkAssessmentService) {
     this.show = false;
+    this.courses = new Array<CourseModal>();
+    this.sections = new Array<SectionModal>();
   }
 
   ngOnInit(): void {
-    this.store.select('fromCourseUpload').subscribe(
+    /*this.store.select('fromCourseUpload').subscribe(
       state => {
         this.info = state.info;
+      }
+    );*/
+    this.markAssessmentService.getCourseForTeacher(1).subscribe(
+      data => {
+        // @ts-ignore
+        for (const course: { SUB_NM: string } of data) {
+          this.courses.push(new CourseModal(course.SUB_NM));
+        }
+        if (this.courses.length > 0) {
+          this.markAssessmentService.getSectionsForTeacherinCourse(1, this.courses[0].courseTitle).subscribe(
+            section => {
+              // @ts-ignore
+              for (const sec: { SECTION: string } of section) {
+                this.sections.push(new SectionModal(sec.SECTION));
+              }
+            }
+          );
+        }
+      }
+    );
+  }
+
+  OnCourseChange(c: HTMLSelectElement) {
+    // Clear previous sections
+    for (const sec of this.sections) {
+      this.sections.pop();
+    }
+
+    // Fetch New Sections on Course Change
+    this.markAssessmentService.getSectionsForTeacherinCourse(1, c.value).subscribe(
+      section => {
+        // @ts-ignore
+        for (const sec: { SECTION: string } of section) {
+          this.sections.push(new SectionModal(sec.SECTION));
+        }
       }
     );
   }
 
   onSubmit(form: NgForm) {
+    // Testing
+    console.log('C:', this.selectCourse.nativeElement.value, this.selectSection.nativeElement.value);
     if (form.invalid) {
       this.show = true;
     } else {
